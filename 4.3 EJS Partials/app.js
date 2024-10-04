6047896c27746bd6e1b2fb3fa2863b70bc86db85
65160268 <65160268@go.buu.ac.th>
2024-10-04 21:57:04 +0700

    Update app.js

--- 4.3 EJS Partials/app.js
+++ 4.3 EJS Partials/app.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import nodemailer from 'nodemailer';
 
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
